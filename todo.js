3b964e8e23fd66ba43948483d430d466cc352e06
talkin <taojing1106@163.com>
2014-03-17 11:26:23 +0800

    add option

--- todo.js
+++ todo.js
@@ -24,11 +24,10 @@ function TodoCtrl($scope) {
     });
   };
 
-  it('should select Greetings!', function() {
-  expect(element(by.id('greet')).getAttribute('selected')).toBeFalsy();
-  element(by.model('selected')).click();
-  expect(element(by.id('greet')).getAttribute('selected')).toBeTruthy();
-  });
+  $scope.classfy = [
+    {classes:'study'},
+    {classes:'enjoy'}
+  ];
 
 
 }
